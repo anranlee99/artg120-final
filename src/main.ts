07137ed1c8ad3a01b35c41dd19bff68d31d6bcc8
Aly <me@s5.pm>
2023-05-16 17:48:20 -0700

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,37 @@
+import 'phaser';
+import * as Tone from 'tone';
+
+class TestScene extends Phaser.Scene {
+    synth: Tone.Synth
+
+    constructor() {
+        super('test');
+
+        this.synth = new Tone.Synth().toDestination()
+    }
+
+    create() {
+        this.input.on(Phaser.Input.Events.POINTER_DOWN, () => {
+            this.synth.triggerAttackRelease("C4", "8n")
+        })
+    }
+}
+
+let game = new Phaser.Game({
+    type: Phaser.WEBGL,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 1920,
+        height: 1080
+    },
+    parent: 'app',
+    title: "ARTG120 Final",
+    scene: [TestScene]
+})
+
+declare global {
+    interface Window { game: Phaser.Game }
+}
+
+window.game = game;
