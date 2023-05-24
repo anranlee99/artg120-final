53cdd490f8e03747df29402cf2909a38111be951
An-Ran Lee <alee208@ucsc.edu>
2023-05-23 17:01:31 -0700

    changes the sync'ing?

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,6 @@ class TestScene extends Phaser.Scene {
         super('test');
 
         this.synth = new Tone.Synth().toDestination()
-        this.synth.sync()
     }
     makeTrack() {
         this.add.rectangle(0, this.h * 0.2, this.w * 0.8, this.h * 0.2, 0xabcdef).setOrigin(0).setStrokeStyle(5, 0)
@@ -33,6 +32,10 @@ class TestScene extends Phaser.Scene {
             this.add.circle(this.w * 0.4 + 200, this.h * 0.1, 10, 0xffffff).setOrigin(0.5),
             this.add.circle(this.w * 0.4 + 300, this.h * 0.1, 10, 0xffffff).setOrigin(0.5),
         ]
+
+        Tone.Transport.stop()
+        Tone.Transport.start()
+
         this.playBtn.on(Phaser.Input.Events.POINTER_DOWN, () => {
             this.playBtn.setAlpha(0.5)
             this.playBtn.disableInteractive()
@@ -41,9 +44,9 @@ class TestScene extends Phaser.Scene {
                 delay: 1000,
                 callback: () => {
                     this.circles[i].setAlpha(0.5)
-                    Tone.Transport.stop()
-                    Tone.Transport.start()
-                    if(i == 3){
+
+                    this.synth.sync()
+                    if (i == 3) {
                         this.synth.triggerAttackRelease("C5", "8n")
                     } else {
                         this.synth.triggerAttackRelease("C4", "8n")
@@ -61,17 +64,17 @@ class TestScene extends Phaser.Scene {
 
         const graphics = this.add.graphics()
         graphics.fillStyle(0x00aaff, 1)
-        let iconRect = this.add.rectangle(this.w*0.9, this.h*0.2, 200, 200).setOrigin(0.5) 
+        let iconRect = this.add.rectangle(this.w * 0.9, this.h * 0.2, 200, 200).setOrigin(0.5)
         let tool = this.add.text(iconRect.x, iconRect.y, "🥁", {
             fontSize: 100
         }).setOrigin(0.5)
-        let mask = graphics.fillRoundedRect(0, 0,  200,200, 32)//.setDepth(iconRect.depth + 1)
+        let mask = graphics.fillRoundedRect(0, 0, 200, 200, 32)//.setDepth(iconRect.depth + 1)
         mask.copyPosition({
             x: iconRect.x - iconRect.width / 2,
             y: iconRect.y - iconRect.height / 2
         })
         iconRect.createGeometryMask(mask)
-        iconRect.setInteractive({  useHandCursor: true,draggable: true })
+        iconRect.setInteractive({ useHandCursor: true, draggable: true })
         iconRect.on(Phaser.Input.Events.DRAG, (pointer: Phaser.Input.Pointer, dragX: number, dragY: number) => {
 
             iconRect.x = dragX
